5f076d0ddf7be5a1ce5e1edfbcfdfa4be7281fb0
pakfrom10 <pakfrom10@gmail.com>
2025-05-14 16:52:48 +0400

    jweojfke

---
+++ main.py
@@ -0,0 +1,13 @@
+age=int(input("please enter your age for sign up "))
+if age>18 and age<50: 
+    print("yes you are qualifyed ")
+elif age>50:
+    d=age%2
+    if d==0:
+        print("your age is qualifyed ")
+elif age<18:
+    c=age%2
+    if c==0:
+        print("your age is qualifyed ")
+else:
+    print("sorry age {} is not allowed for sign up".format(age))
